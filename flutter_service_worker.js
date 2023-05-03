72888850351bc0383b8ecc15944064f5bdf5c8d7
Ziad <zhussein1996@gmail.com>
2023-05-03 16:54:59 -0400

    more verse bug fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "eb558f70129ad71d63c3367e0767ad4a",
-"/": "eb558f70129ad71d63c3367e0767ad4a",
-"main.dart.js": "47af84f5a99c7307f480fa8c11a26d2c",
+"index.html": "648e69c52ba387777a23974cbda4871c",
+"/": "648e69c52ba387777a23974cbda4871c",
+"main.dart.js": "9640919d95b2953120b1eda40f14bbf2",
 "manifest.json": "f4bc405a6a8352fed77311de35cc327e",
 "version.json": "1ada29d6f7e63e6ff91fa8d10a37d9bb"
 };
